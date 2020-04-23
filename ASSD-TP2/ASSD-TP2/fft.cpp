c1913db30f8e25756680a8e2ab2265e1baea5aaf
ffarall <37149322+ffarall@users.noreply.github.com>
2020-04-23 16:57:54 -0300

    missing calculation of Wn

--- ASSD-TP2/ASSD-TP2/fft.cpp
+++ ASSD-TP2/ASSD-TP2/fft.cpp
@@ -1,5 +1,44 @@
 #include "fft.h"
+#include <cmath>
 
-void fft(complex<double>* in, complex<double>* out, size_t n)
+void FFTCalculator::FFT(complex<double>* in, complex<double>* out, size_t n)
 {
+	// Calculating Wn.
+	complex<double>* Wn;
+	
+	recursiveFFT(out, Wn, n, n, 0, 0);
+}
+
+void FFTCalculator::recursiveFFT(complex<double>* Xk, complex<double>* Wn, size_t N, size_t totalN, int groupNumber, int level)
+{
+	unsigned int offsetInXn = N * groupNumber;
+	unsigned int currentWn = bitReverse(sqrt(totalN), groupNumber);
+
+	// Computing FFT for this group.
+	for (size_t i = 0; i < N/2; i++)
+	{
+		complex<double> auxXk = Xk[offsetInXn + i];
+		Xk[offsetInXn + i] = Xk[offsetInXn + i] + Wn[currentWn + i] * Xk[offsetInXn + N / 2 + i];
+		Xk[offsetInXn + N / 2 + i] = auxXk - Wn[currentWn + i] * Xk[offsetInXn + N / 2 + i];
+	}
+
+	// Recursive case
+	if (N != 2)
+	{
+		recursiveFFT(Xk, Wn, N / 2, totalN, 2 * groupNumber, level + 1);
+		recursiveFFT(Xk, Wn, N / 2, totalN, 2 * groupNumber + 1, level + 1);
+	}
+}
+
+unsigned int FFTCalculator::bitReverse(size_t numberOfBits, unsigned int num)
+{
+	unsigned int reversed = 0;
+	for (size_t i = 0; i < numberOfBits; i++)
+	{
+		if (num & (1 << i))
+		{
+			reversed |= 1 << ((numberOfBits - 1) - i);
+		}
+	}
+	return reversed;
 }
--- ASSD-TP2/ASSD-TP2/fft.h
+++ ASSD-TP2/ASSD-TP2/fft.h
@@ -3,4 +3,12 @@
 
 using namespace std;
 
-void fft(complex<double>* in, complex<double>* out, size_t n);
+class FFTCalculator
+{
+public:
+	void FFT(complex<double>* in, complex<double>* out, size_t n);
+
+private:
+	void recursiveFFT(complex<double>* Xk, complex<double>* Wn, size_t N, size_t totalN, int groupNumber, int level);
+	unsigned int bitReverse(size_t numberOfBits, unsigned int num);
+};
