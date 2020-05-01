af49b9cea67a0476bc6d3b266b5aff5ef469f779
Malena Müller <malena@Malenas-MacBook-Pro.local>
2020-05-01 18:53:41 -0300

    info midi

--- ProgramaPrincipal/main.py
+++ ProgramaPrincipal/main.py
@@ -1,9 +1,18 @@
 from mido import MidiFile
 
-mid = MidiFile('Resources/Movie_Themes_-_Toy_Story.mid', clip=True)
+mid = MidiFile('Resources/Rodrigo_-_2do_movimiento_Concierto_de_Aranjuez__Adagio.mid', clip=True)
+mid1 = MidiFile('Resources/Movie_Themes_-_Star_Wars_-_by_John_Willams.mid', clip=True)
+mid2 = MidiFile('Resources/Movie_Themes_-_Toy_Story.mid', clip=True)
+
+
+#type 0 (single track): all messages are saved in one track
+#type 1 (synchronous): all tracks start at the same time
+#type 2 (asynchronous): each track is independent of the others
 print(mid)
-for i, track in enumerate(mid.tracks):
-    print('Track {}: {}'.format(i, track.name))
+for track in mid.tracks:
+    print(track)
+
+for track in mid.tracks:
+    print(track)
     for msg in track:
         print(msg)
-
