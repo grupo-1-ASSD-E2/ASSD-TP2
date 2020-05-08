d96b0ef41298c901d7c1e7f269ec227b2ea082bd
gonzadavidov <gdavidov@itba.edu.ar>
2020-05-08 01:35:52 -0300

    u.u

--- ProgramaPrincipal/BackEnd/SamplesBasedSynthesis/SBSynthesis.py
+++ ProgramaPrincipal/BackEnd/SamplesBasedSynthesis/SBSynthesis.py
@@ -1,15 +1,16 @@
 from BackEnd.SynthesizerAbstract import SynthesizerAbstract
-from BackEnd.SamplesBasedSynthesis.SampleFrequencies import get_freq_from_fft
 from BackEnd.SamplesBasedSynthesis.SampleAdapting import *
 import numpy as np
 import soundfile as sf
 import os
+from scipy.io.wavfile import write
 
 
 class SB_Synthesizer(SynthesizerAbstract):
 
     def __init__ (self):
         self.samples_directory = 'ProgramaPrincipal/BackEnd/SamplesBasedSynthesis/samples/'
+        self.existing_frec_dict()
         self.my_samples_frecuencies()
 
     def synthesize_track(self, track):
@@ -21,10 +22,18 @@ class SB_Synthesizer(SynthesizerAbstract):
         midi_code_note = self.midi_code_from_frec(note.frequency)
         midi_code_closest_note = self.midi_code_from_frec(self.samples_frec_dic[closest_note])
         shift = midi_code_note - round(midi_code_closest_note)
-        end = note_scaling(self.data, self.samplerate, shift)
-        zeros = [0]*(440118 - len(end))
-        return np.concatenate((end,zeros))
+        data, samplerate = sf.read(self.samples_directory + closest_note)
+        pitched_note = note_scaling(data, samplerate, shift)
 
+        note_length = len(np.linspace(0, note.duration, num=(time_base.fs * note.duration)))
+        time_stretched_note = time_stretch(pitched_note, len(pitched_note) / (note_length - 2**11)) #Creates array of specified length
+
+        amp_values = np.array(time_base.get_time_array())
+        amp_values = [0] * len(amp_values)
+        return time_stretched_note
+        #zeros = [0]*(440118 - len(end))
+        #return np.concatenate((end,zeros))
+        
 
     def midi_code_from_frec(self, frec):
         '''
@@ -46,20 +55,24 @@ class SB_Synthesizer(SynthesizerAbstract):
         This method creates a dictionary with samples as key and frecuencies as value
         '''
         self.samples_frec_dic = {}
-        '''
         for sample in os.listdir(self.samples_directory):
-            try:
-                self.data, self.samplerate = sf.read(self.samples_directory + sample)
-                self.samples_frec_dic[sample] = get_freq_from_fft(self.data, self.samplerate)
-                self.mono = True
-            except Exception as e:
-                self.data = self.data.sum(axis=1) / 2 #convert data from stereo to mono
-                self.data, self.samplerate = sf.read(self.samples_directory + sample)
-                self.samples_frec_dic[sample] = get_freq_from_fft(self.data, self.samplerate)
-                self.mono = False
-        '''
-        for sample in os.listdir(self.samples_directory):
-            self.data, self.samplerate = sf.read(self.samples_directory + sample)
-            self.data = self.data.sum(axis=1) / 2
-            self.samples_frec_dic[sample] = get_freq_from_fft(self.data, self.samplerate)
+            note_from_file = sample.split(".")        #Remember to always have the sample file in the following format .note.extension
+            self.samples_frec_dic[sample] = self.existing_frec[note_from_file[-2]]
 
+    def existing_frec_dict(self):
+        self.existing_frec = {}
+        letters = ["A0", "Bb0", "B0",
+                "C1", "Db1", "D1", "Eb1", "E1", "F1", "Gb1", "G1", "Ab1", "A1", "Bb1", "B1",
+				"C2", "Db2", "D2", "Eb2", "E2", "F2", "Gb2", "G2", "Ab2", "A2", "Bb2", "B2",
+				"C3", "Db3", "D3", "Eb3", "E3", "F3", "Gb3", "G3", "Ab3", "A3", "Bb3", "B3",
+				"C4", "Db4", "D4", "Eb4", "E4", "F4", "Gb4", "G4", "Ab4", "A4", "Bb4", "B4",
+				"C5", "Db5", "D5", "Eb5", "E5", "F5", "Gb5", "G5", "Ab5", "A5", "Bb5", "B5",
+				"C6", "Db6", "D6", "Eb6", "E6", "F6", "Gb6", "G6", "Ab6", "A6", "Bb6", "B6",
+				"C7", "Db7", "D7", "Eb7", "E7", "F7", "Gb7", "G7", "Ab7", "A7", "Bb7", "B7",
+				"C8"] 
+        i=0
+        for note in letters:
+
+            prev_freq = round(440/32*(2**(((21+i)-9)/12)), 3)
+            self.existing_frec[note] = prev_freq
+            i += 1
--- ProgramaPrincipal/BackEnd/SamplesBasedSynthesis/SampleAdapting.py
+++ ProgramaPrincipal/BackEnd/SamplesBasedSynthesis/SampleAdapting.py
@@ -3,74 +3,49 @@ import numpy as np
 import soundfile as sf 
 import sys
 
-######################################################################################
-"""
-=> Definition: note_scaling()
-=> Description: This function initiates the call to start shifting the pitch of the 
-               .WAV file and creates a Sample folder to hold the created pitch-shifted
-               .WAV files
-=> Parameters: 
-=>        - input_data: numpy array representing the sample data of the .WAV file
-         - input_samp: the sampling rate of the .WAV file
-         - outputfolder: desired name of the output folder that is created
-         - shifts: array of integers representing the number of shifts to be created.
-                   Ex: [-2, -1, 1, 2] will create new .WAV samples pitch-shifted by 
-                   2 semitones down, 1 semitone down, 1 semitone up, and 2 semitones up 
-=> Precondition: Assumes that the input data is of a valid WavFile object, and shifts is a valid
-                range array
-=> Postcondition: Creates a folder to the specified location by outputfolder, containing the new .WAV files
-                 Returns the name/location of the output folder         
-"""     
-######################################################################################
+
 def note_scaling(input_data, input_samp, shift):
-    transposed = pitch_shift(input_data, shift)
-    return transposed.astype(input_data.dtype)
+	'''
+	This function initiates the call to start shifting the pitch by the factor shift.
+	Returns: pitched sound as array
+	'''
 
+	transposed = pitch_shift(input_data, shift)
+	return transposed.astype(input_data.dtype)
 
-######################################################################################
-"""
-=> Definition: change_speed()
-=> Description: This function changes the speed of playback of the .WAV file by some factor. This
-                accomplishes the shift in pitch. 
-=> Parameters: 
-=>       - wav_data: numpy array representing the sample data of the .WAV file
-         - factor: The factor at which the speed of the sound will be changed. 
-=> Precondition: Assumes that the input data is of a valid WavFile object, and that the factor is a real and rational number.
-=> Postcondition: Returns a new wav_data array that holds values closest to those spaced out by the input factor         
-"""     
-######################################################################################
-def change_speed(wav_data, factor): 
-	indices = np.round(np.arange(0, len(wav_data), factor)) #create an even-spaced array (wav_data) spaced by the factor. Ex. if factor was 2.333 it would change (1, 2, 3, 4, etc.) to (0, 2.333, 4.666 etc.)
-	indices = indices[indices < len(wav_data)].astype(int) #astype int takes the neghboring values to these, but then preserves the same vector length. 
-	return wav_data[indices.astype(int)]
+def pitch_shift(input_data, n, DFT_size = 2**11, hop_size=2048/8):
+	'''
+	This function begins to change the pitch of the sound by implementing the Phase Vocoder method. 
+	That is first time-stretching the sound, then speedh changing it all by a common factor.
+	'''
+	#changes the pitch of a sound by n semitones
+	factor = 2**(1.0 * n / 12.0)
+	stretched = time_stretch(input_data, 1.0/factor, DFT_size, hop_size)
+	return change_speed(stretched[DFT_size:], factor)
 
-######################################################################################
-"""
-=> Definition: time_stretch()
-=> Description: This function stretches the input .WAV file by an input factor. Stretching the sound while maintaining its pitch
-=> Parameters: 
-=>       - wav_data: numpy array representing the sample data of the .WAV file
-         - factor: The factor at which the duration of the sound will be changed
-         - DFT_size: The size of the Discrete Fourier Transform window that will  be applied to the signal
-         - hop_size: The corresponding hop sizes between each consecutive frame of the signal 
-=> Precondition: Assumes that the input data is of a valid WavFile object, and that the factor is a real and rational number.
-                 Also assumes that the DFT_size and hop_size are both represented in bits, and are also both real and rational numbers
-                 that are less than the length of the orginal sound.
-=> Postcondition: Returns a reconstructed sequence of samples that is scaled to match the original amplitude of the signal at each 
-                  sample index.          
-"""     
-######################################################################################
-def time_stretch(wav_data, factor, DFT_size, hop_size):
-	#stretches the sound by a factor 
+def change_speed(input_data, factor): 
+	'''
+	This function changes the speed of playback of the .WAV file by some factor. It is used with pitching to accomodate the speed.
+	'''
+
+	indices = np.round(np.arange(0, len(input_data), factor)) #Create an even-spaced array (input_data) spaced by the factor.
+	indices = indices[indices < len(input_data)].astype(int) #Astype int takes the neghboring values to these, but then preserves the same vector length. 
+	return input_data[indices.astype(int)]
+
+def time_stretch(input_data, factor, DFT_size=2**11, hop_size=2048/8):
+	'''
+	This function stretches the input file by an input factor maintaining its pitch.
+	Returns: Array of the note shifted by factor.
+	'''
 	
-	L = len(wav_data)
+	L = len(input_data)
 	#set up our signal arrays to hold the processing output
 	phi = np.zeros(DFT_size) #create an array of zeros (float) of DFT_size
 	out = np.zeros(DFT_size, dtype = complex)
-	signal_out = np.zeros(int(L/factor) + DFT_size, dtype = complex)
+	signal_out = np.zeros(int(round(L/factor)) + DFT_size, dtype = complex)
 
 	#Find out what the peak amplitude of input is (for scaling) and create a hanning window
-	amp = max(wav_data)
+	amp = max(input_data)
 	hanning_window = np.hanning(DFT_size)
 
 	p = 0
@@ -79,8 +54,8 @@ def time_stretch(wav_data, factor, DFT_size, hop_size):
 
 		#take the spectra of two consecutive windows
 		p1 = int(p)
-		spectra_1 = np.fft.fft(hanning_window * wav_data[p1: p1 + int(DFT_size)])
-		spectra_2 = np.fft.fft(hanning_window * wav_data[p1 + int(hop_size): p1 + int(DFT_size) + int(hop_size)])
+		spectra_1 = np.fft.fft(hanning_window * input_data[p1: p1 + int(DFT_size)])
+		spectra_2 = np.fft.fft(hanning_window * input_data[p1 + int(hop_size): p1 + int(DFT_size) + int(hop_size)])
 
 		#take their phase difference and integrate
 		phi += (np.angle(spectra_2) - np.angle(spectra_1)) #about aligning two nieghboring spectra, by looking at the relative shifts of phase at each frequency bin - based on their phase difference 
@@ -93,33 +68,12 @@ def time_stretch(wav_data, factor, DFT_size, hop_size):
 
 		out.real, out.imag = np.cos(phi), np.sin(phi)
 		#inverse FFT and overlap-add to reconstruct the sequence
-		signal_out[pp:pp+int(DFT_size)] += (hanning_window * np.fft.ifft(np.abs(spectra_2)*out)) #using overlapping and blending - provides a smooth transition between two neighboring windows
+		signal_out[pp:pp+int(DFT_size)] += (hanning_window * np.fft.ifft(np.abs(spectra_2)*out)) 
 		pp += int(hop_size)
 		p += hop_size*factor
 
 	#write the output and scale it to match the original amplitude
 	signal_out = amp*signal_out/max(signal_out)
 
-	return signal_out
+	return signal_out.astype(input_data.dtype)
 
-######################################################################################
-"""
-=> Definition: pitch_shift()
-=> Description: This function begins to change the pitch of the sound by implementing
-                the Phase Vocoder method. That is first time-stretching the sound, then 
-                speedh changing it all by a common factor.
-=> Parameters: 
-=>       - wav_data: numpy array representing the sample data of the .WAV file
-         - n: The amount of semitones to change the pitch of the signal by 
-=> Precondition: Assumes that the input data is of a valid WavFile object, and that n comes from the values
-                 inside of a valid range array
-=> Postcondition: Returns a new wav_data array that holds values closest to those spaced out by the input factor
-                  from the time stretched data array         
-"""     
-######################################################################################
-def pitch_shift(wav_data, n, DFT_size = 2**11, hop_size=2048/8):
-	#changes the pitch of a sound by n semitones
-	factor = 2**(1.0 * n / 12.0)
-	stretched = time_stretch(wav_data, 1.0/factor, DFT_size, hop_size)
-	#print(stretched)
-	return change_speed(stretched[DFT_size:], factor)
