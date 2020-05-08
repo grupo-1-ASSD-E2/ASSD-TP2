dbdf71614596df8d6c27c365821d78d61065204b
gonzadavidov <gdavidov@itba.edu.ar>
2020-05-08 17:28:00 -0300

    instrumentos

---
+++ ProgramaPrincipal/BackEnd/Instruments/Banjo.py
@@ -0,0 +1,6 @@
+from BackEnd.SamplesBasedSynthesis.SBSynthesis import SB_Synthesizer
+
+class Banjo:
+    def __init__(self, synthesizer):
+        self.synthesizer = synthesizer
+        self.instrument_name = 'Banjo'
---
+++ ProgramaPrincipal/BackEnd/Instruments/Cello.py
@@ -0,0 +1,6 @@
+from BackEnd.SamplesBasedSynthesis.SBSynthesis import SB_Synthesizer
+
+class Cello:
+    def __init__(self, synthesizer):
+        self.synthesizer = synthesizer
+        self.instrument_name = 'Cello'
