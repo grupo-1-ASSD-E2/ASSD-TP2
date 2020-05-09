f05a0480972ef8a944022ccf09b12023f372d5d6
gonzadavidov <gdavidov@itba.edu.ar>
2020-05-08 21:39:33 -0300

    debug 100000.0

--- ProgramaPrincipal/BackEnd/BackEnd.py
+++ ProgramaPrincipal/BackEnd/BackEnd.py
@@ -18,13 +18,14 @@ class BackEnd:
         self.ks_synthesizer = KS_Synthesizer()
         self.sb_synthesizer = SB_Synthesizer()
         self.song = Song()
-        #song.load_midi_file_info('ProgramaPrincipal/Resources/Movie_Themes_-_Toy_Story.mid')
-        self.song.load_midi_file_info('ProgramaPrincipal/Resources/Disney_Themes_-_Under_The_Sea.mid')
+        #self.song.load_midi_file_info('ProgramaPrincipal/Resources/Movie_Themes_-_Toy_Story.mid')
+        #self.song.load_midi_file_info('ProgramaPrincipal/Resources/Disney_Themes_-_Under_The_Sea.mid')
+        self.song.load_midi_file_info('ProgramaPrincipal/Resources/Movie_Themes_-_Star_Wars_-_by_John_Willams.mid')
         self.midi_path = 'ProgramaPrincipal/Resources/'
         #PARA PROBAR
         self.song.tracks[0].assign_instrument('Trumpet')
         self.synthesize_track(self.song.tracks[0])
-        self.play_song(self.song.tracks[0].output_signal)
+        self.play_signal(self.song.tracks[0].output_signal)
 
     def assign_midi_path(self, midi_file_name):
         self.song.load_midi_file_info(self.midi_path + midi_file_name)
@@ -49,7 +50,7 @@ class BackEnd:
     def synthesize_track(self, track):
         for note in track.notes:
             self.synthesize_note(note, track.instrument)
-        track.output_singal = self.generate_output_signal(track.time_base.timeline_length, track.notes, track.time_base.fs)
+        track.output_signal = self.generate_output_signal(track.time_base.timeline_length, track.notes, track.time_base.fs)
 
     def syntesize_entire_song(self, song):
         for track in song.tracks:
@@ -62,9 +63,10 @@ class BackEnd:
         for i in arrays_to_add:
             subarray = i.output_signal
             init_time_index = int(round(i.initial_time * fs))
-            index_difference = np.zeros(init_time_index - len(output))
-            if init_time_index > len(output):
-                output = np.concatenate((output, index_difference))
+            index_difference = init_time_index - len(output)
+            zero_padd = np.zeros(index_difference)
+            if init_time_index >= len(output):
+                output = np.concatenate((output, zero_padd))
                 output = np.concatenate((output, subarray))
             else:
                 superpose , add = np.split(subarray, abs(index_difference))[0],np.split(subarray, abs(index_difference))[1]
--- ProgramaPrincipal/BackEnd/TimeBase/Tempo.py
+++ ProgramaPrincipal/BackEnd/TimeBase/Tempo.py
@@ -6,7 +6,7 @@ class Tempo:
         self.delta_ticks = delta_ticks
         self.start_tick = start_tick
         self.end_tick = start_tick + delta_ticks
-        self.seconds_per_tick = int((self.tempo / self.ticks_per_beat) * 1000000) #self.__get_seconds_per_tick__(self.tempo, self.ticks_per_beat)
+        self.seconds_per_tick = (self.tempo / self.ticks_per_beat) * 10**-6 #self.__get_seconds_per_tick__(self.tempo, self.ticks_per_beat)
         self.total_duration_of_tempo = self.seconds_per_tick * self.delta_ticks #duration in seconds!!
         print('total duration of tempo:', self.total_duration_of_tempo)
 
